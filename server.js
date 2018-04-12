c9ee6d72d09b687f921ae5e5e242a98ea315f0d8
Jason McCutchan <jasonmccutchan@Jasons-MacBook-Pro.local>
2018-04-11 21:39:00 -0400

    changed port

--- server.js
+++ server.js
@@ -235,11 +235,9 @@ app.use(webpackDevMiddleware(compiler, {
 }));
 
 
-console.log('Hiya')
 
+   app.set('port', process.env.PORT || 3000)
 
-const server = app.listen(3000, function() {
+  const server = app.listen(app.get('port'))
   const host = server.address().address;
   const port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
-})
